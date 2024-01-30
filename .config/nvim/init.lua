9e4c43f215118b1fa6543f7cb5450565136a278c
Jim Rybarski <jim@rybarski.com>
2024-01-30 07:47:22 -0500

    diagnostics

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -51,11 +51,26 @@ vim.g.completopt = "menu,menuone,preview,noinsert,noselect"
 -- keep the cursor in the center of the screen (if there is space)
 vim.o.scrolloff = 10
 
+-- highlight the 120th column
+vim.opt.colorcolumn = "120"
+
+-- autoindent in codebases
+vim.opt.smartindent = true
+vim.opt.shiftwidth = 4
+
+-- wrapped text starts at the same level of indentation as the first line
+vim.opt.breakindent = true
+
+-- open new splits down or to the right
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
 -- transparently edit compressed files
 vim.g.loaded_gzip = 1
 vim.g.loaded_tar = 1
 vim.g.loaded_zipPlugin = 1
 
+vim.g.python3_host_prog = vim.fn.expand("$HOME/.local/pylspenv/bin/python3")
 
 -- Make a shorter alias for some commands
 local autocmd = vim.api.nvim_create_autocmd
@@ -78,16 +93,22 @@ autocmd("FileType", {
 
 -- load plugins
 require("lazy").setup{
+    -- none-ls and others depend on plenary
+    { "nvim-lua/plenary.nvim" },
+    { "nvimtools/none-ls.nvim" },
+    -- { "folke/trouble.nvim",
+    --     dependencies = { "nvim-tree/nvim-web-devicons" },
+    -- },
+    { "neovim/nvim-lspconfig" },
     {"L3MON4D3/LuaSnip"},
     {"saadparwaiz1/cmp_luasnip"},
     { "hrsh7th/cmp-calc" },
     { "max397574/cmp-greek" },
     { "chrisgrieser/cmp-nerdfont" },
     { "ray-x/cmp-treesitter" },
-    {"hrsh7th/cmp-nvim-lua"},
     {"rcarriga/nvim-notify"},
+    {"hrsh7th/cmp-nvim-lsp"},
     {"hrsh7th/cmp-buffer"},
-    -- {"hrsh7th/cmp-nvim-lsp"},
     {"hrsh7th/cmp-path"},
     {"hrsh7th/nvim-cmp"},
     {"windwp/nvim-autopairs"},
@@ -155,11 +176,27 @@ require("lazy").setup{
           end
     }, 
     {"RRethy/vim-illuminate"},
-    { "kevinhwang91/promise-async" },
-    { "kevinhwang91/nvim-ufo" },
+    { "kevinhwang91/nvim-ufo",
+        dependencies = { "kevinhwang91/promise-async" },
+    },
     {"lunarvim/bigfile.nvim"},
     {"hrsh7th/cmp-emoji"},
 }
+
+-- configure linters and formatters
+null_ls = require("null-ls")
+null_ls.setup({
+  sources = {
+    null_ls.builtins.diagnostics.pylint,
+    null_ls.builtins.formatting.isort,
+    null_ls.builtins.diagnostics.mypy,
+    null_ls.builtins.formatting.ruff,
+    null_ls.builtins.formatting.ruff_format,
+    null_ls.builtins.formatting.rustfmt,
+    null_ls.builtins.diagnostics.shellcheck,
+    null_ls.builtins.diagnostics.fish,
+  },
+})
 
 local luasnip = require("luasnip")
 
@@ -227,11 +264,7 @@ cmp.setup({
         end, {"i", "s"}),
         ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
-                if luasnip.expand_or_jumpable() then
-                    cmp.select_next_item()
-                else
-                    cmp.select_next_item({behavior = cmp.SelectBehavior.Insert})
-                end
+                cmp.select_next_item({behavior = cmp.SelectBehavior.Insert})
             else
                 fallback()
             end
@@ -275,13 +308,16 @@ cmp.setup({
         ['<C-k>'] = cmp.mapping.scroll_docs(-4),
         ['<C-j>'] = cmp.mapping.scroll_docs(4),
         },
+        
         sources = cmp.config.sources({
-            { name = 'luasnip' },
+            { name = 'nvim_lsp' },
+            { name = 'luasnip', max_item_count = 5 },
             { name = 'treesitter' },
-            { name = 'buffer' },
-            { name = 'path'},
+            { name = 'buffer', max_item_count = 5 },
+            { name = 'path', max_item_count = 5 },
             { name = 'greek',
-            option = { insert = true } },
+            option = { insert = true },
+            max_item_count = 1 },
             { name = 'nerdfont', 
               option = { insert = true } },
             { name = 'emoji' , 
@@ -579,321 +615,46 @@ vim.api.nvim_set_keymap("o", "\\", ":HopChar1<CR>", {noremap = true})
 
 require("nvim-surround").setup()
 
---     "nvim-lua/plenary.nvim", 
---     "tamago324/nlsp-settings.nvim",
---     "neovim/nvim-lspconfig", {
---         "nvim-telescope/telescope.nvim",
---         config = function()
---             require("telescope").setup({
---                 defaults = {
---                     vimgrep_arguments = {
---                         "rg", "--color=never", "--no-heading",
---                         "--with-filename", "--line-number", "--column",
---                         "--smart-case", "--hidden", "--glob=!.git/"
---                     },
---                     path_display = {"smart"},
---                     border = {},
---                     borderchars = nil,
---                     color_devicons = true,
---                     set_env = {["COLORTERM"] = "truecolor"}
---                 }
---             })
---         end
---     }, {"nvim-telescope/telescope-fzf-native.nvim", build = "make"}, 
---
---     
---     {"klen/nvim-test"},
---     {"mfussenegger/nvim-dap"},
---     
---     {"ray-x/lsp_signature.nvim"},
---     {"rcarriga/nvim-dap-ui"},
--- }, {})
---
--- require("nvim-test").setup {}
---
---
--- require("lsp_signature").setup({
---     debug = false, -- set to true to enable debug logging
---     log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log", -- log dir when debug is on
---     -- default is  ~/.cache/nvim/lsp_signature.log
---     verbose = false, -- show debug line number
---     bind = true, -- This is mandatory, otherwise border config won't get registered.
---     -- If you want to hook lspsaga or other signature handler, pls set to false
---     doc_lines = 10, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
---     -- set to 0 if you DO NOT want any API comments be shown
---     -- This setting only take effect in insert mode, it does not affect signature help in normal
---     -- mode, 10 by default
---     max_height = 12, -- max height of signature floating_window
---     max_width = 80, -- max_width of signature floating_window, line will be wrapped if exceed max_width
---     -- the value need >= 40
---     wrap = true, -- allow doc/signature text wrap inside floating_window, useful if your lsp return doc/sig is too long
---     floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
---     floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
---     -- will set to true when fully tested, set to false will use whichever side has more space
---     -- this setting will be helpful if you do not want the PUM and floating win overlap
---
---     floating_window_off_x = 1, -- adjust float windows x position.
---     -- can be either a number or function
---     floating_window_off_y = 0, -- adjust float windows y position. e.g -2 move window up 2 lines; 2 move down 2 lines
---     -- can be either number or function, see examples
---
---     close_timeout = 4000, -- close floating window after ms when laster parameter is entered
---     fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
---     hint_enable = true, -- virtual hint enable
---     hint_prefix = "> ",
---     hint_scheme = "String",
---     hint_inline = function() return false end, -- should the hint be inline(nvim 0.10 only)?  default false
---     -- return true | 'inline' to show hint inline, return 'eol' to show hint at end of line, return false to disable
---     -- return 'right_align' to display hint right aligned in the current line
---     hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
---     handler_opts = {
---         border = "double" -- double, rounded, single, shadow, none, or a table of borders
---     },
---
---     always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
---
---     auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
---     extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
---     zindex = 200, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
---
---     padding = '', -- character to pad on left and right of signature can be ' ', or '|'  etc
---
---     transparency = nil, -- disabled by default, allow floating win transparent value 1~100
---     shadow_blend = 36, -- if you using shadow as border use this set the opacity
---     shadow_guibg = 'Black', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
---     timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
---     toggle_key = nil, -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
---     toggle_key_flip_floatwin_setting = false, -- true: toggle floating_windows: true|false setting after toggle key pressed
---     -- false: floating_windows setup will not change, toggle_key will pop up signature helper, but signature
---     -- may not popup when typing depends on floating_window setting
---
---     select_signature_key = nil, -- cycle to next signature, e.g. '<M-n>' function overloading
---     move_cursor_key = nil -- imap, use nvim_set_current_win to move cursor between current win and floating
--- })
---
--- local cmp = require("cmp")
--- local cmp_types = require('cmp.types')
+local lspconfig = require('lspconfig')
+local lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
+local capabilities = require('cmp_nvim_lsp').default_capabilities(lsp_capabilities)
 
--- cmp.setup({
---     -- Specify the completion behavior
---     completion = {completeopt = 'menu,menuone,noselect'},
---
---     -- Disable preselecting the first completion item
---     preselect = cmp.PreselectMode.None,
---
---     -- Mapping for keyboard shortcuts
---     mapping = {
---         ['<C-n>'] = cmp.mapping(function(fallback)
---             if luasnip.jumpable(1) then luasnip.jump(1) end
---         end, {"i", "s"}),
---         ['<C-p>'] = cmp.mapping(function(fallback)
---             if luasnip.jumpable(-1) then luasnip.jump(-1) end
---         end, {"i", "s"}),
---         ['<Tab>'] = cmp.mapping(function(fallback)
---             if cmp.visible() then
---                 if luasnip.expand_or_jumpable() then
---                     cmp.select_next_item({})
---                 else
---                     cmp.select_next_item({behavior = cmp.SelectBehavior.Insert})
---                 end
---             else
---                 fallback()
---             end
---         end, {"i", "s"}),
---
---         ['<S-Tab>'] = cmp.mapping(function(fallback)
---             if cmp.visible() then
---                 if luasnip.expand_or_jumpable() then
---                     cmp.select_prev_item({})
---                 else
---                     cmp.select_prev_item({behavior = cmp.SelectBehavior.Insert})
---                 end
---             else
---                 fallback()
---             end
---         end, {"i", "s"}),
---
---         ['<CR>'] = cmp.mapping(function(fallback)
---             local entry = cmp.get_selected_entry()
---             if entry == nil then
---                 -- for some reason, after entering a snippet and pressing enter, this mapping still
---                 -- gets called, but entry will be nil, in which case we just fallback, after which
---                 -- we'll have left the autocomplete context and everything returns to normal.
---                 -- Is there a more elegant way to fix this? Probably!
---                 fallback()
---             elseif entry:get_kind() == cmp_types.lsp.CompletionItemKind.Snippet then
---                 -- Insert the snippet and escape from autocomplete mode
---                 cmp.confirm({select = true})
---                 cmp.abort()
---                 fallback()
---             elseif luasnip.expand_or_jumpable() then
---                 luasnip.expand_or_jump()
---             end
---             if cmp.visible() then cmp.close() end
---             fallback()
---         end, {"i", "s"}),
---
---         ['<Esc>'] = cmp.mapping(function(fallback)
---             if cmp.visible() then
---                 local entry = cmp.get_selected_entry()
---                 if not entry then
---                     cmp.abort()
---                     fallback()
---                 else
---                     fallback()
---                 end
---             else
---                 fallback()
---             end
---         end, {"i", "s"})
---     },
---
---     -- Define the sources for completion
---     sources = cmp.config.sources({
---         {name = 'nvim_lsp'}, {name = 'luasnip'}, {name = 'buffer'},
---         {name = 'path'}
---     })
--- })
---
--- local on_attach = function(client, bufnr)
---     client.config.flags.debounce_text_changes = 150
---     local opts = {noremap = true, silent = true}
---     local buf_set_keymap = vim.api.nvim_buf_set_keymap
---     buf_set_keymap(bufnr, 'n', 'gd', ':Telescope lsp_definitions<CR>', opts)
---     buf_set_keymap(bufnr, 'n', 'gr', ':Telescope lsp_references<CR><Esc>', opts)
---     buf_set_keymap(bufnr, 'n', 'go', ':Telescope diagnostics<CR><Esc>', opts)
---     buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
---     buf_set_keymap(bufnr, 'n', 'g[', '<cmd>lua vim.diagnostic.goto_next()<CR>',
---                    opts)
---     buf_set_keymap(bufnr, 'n', 'g]', '<cmd>lua vim.diagnostic.goto_prev()<CR>',
---                    opts)
---     buf_set_keymap(bufnr, 'n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
---     buf_set_keymap(bufnr, 'n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
---     buf_set_keymap(bufnr, 'n', 'gs',
---                    '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
--- end
--- local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- local lspconfig = require('lspconfig')
--- -- lspconfig.pylsp.setup {
--- --     cmd = {vim.fn.expand('$HOME/.local/pylspenv/bin/pylsp')},
--- --     capabilities = capabilities,
--- --     on_attach = on_attach
--- -- }
--- require('lspconfig').ruff_lsp.setup {
---   on_attach = on_attach,
---   cmd = { vim.fn.expand('$HOME/.local/pylspenv/bin/ruff-lsp') },
---   init_options = {
---     settings = {
---       args = {},
---     }
---   }
--- }
---
--- -- lspconfig.jedi_language_server.setup({
--- --     cmd = { vim.fn.expand("$HOME/.local/pylspenv/bin/jedi-language-server")}
--- -- })
---
--- lspconfig.rust_analyzer.setup({
---     capabilities = capabilities,
---     on_attach = on_attach,
---     settings = {["rust-analyzer"] = {checkOnSave = {command = "clippy"}}}
--- })
---
--- vim.api.nvim_create_autocmd("CursorHold", {
---     callback = function()
---         local opts = {
---             focusable = false,
---             close_events = {
---                 "BufLeave", "CursorMoved", "InsertEnter", "FocusLost"
---             },
---             border = 'rounded',
---             source = 'always',
---             prefix = ' ',
---             scope = 'cursor'
---         }
---         vim.diagnostic.open_float(nil, opts)
---     end
--- })
---
+lspconfig.jedi_language_server.setup({
+    cmd = { vim.fn.expand("$HOME/.local/pylspenv/bin/jedi-language-server")},
+    capabilities = capabilities,
+})
 
--- local dap = require('dap')
---
--- dap.adapters.python = {
---     type = 'executable',
---     command = get_python_path(),
---     args = {'-m', 'debugpy.adapter'}
--- }
---
--- dap.configurations.python = {
---     {
---         type = 'python',
---         request = 'launch',
---         name = "Launch file",
---         console = "integratedTerminal",
---         redirectOutput = true,
---
---         -- Adjust the program path to point to your specific main Python file
---         program = "${file}",
---         pythonPath = function() return get_python_path() end
---     }
--- }
---
--- local dapui = require('dapui')
--- dapui.setup({
---     layouts = {
---         {
---             elements = {"scopes", "breakpoints", "stacks", "watches"},
---             size = 40,
---             position = "left" -- Can be "left", "right", "top", "bottom"
---         }, {elements = {"console"}, size = 16, position = "bottom"}
---     }
--- })
---
--- dap.listeners.after.event_initialized["dapui_config"] =
---     function() dapui.open() end
--- dap.listeners.before.event_terminated["dapui_config"] =
---     function() dapui.close() end
--- dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
---
---
---
--- vim.o.background = "dark"
--- 
+lspconfig.rust_analyzer.setup({
+    capabilities = capabilities,
+    settings = {
+        ["rust-analyzer"] = {
+            checkOnSave = {
+                command = "clippy" }
+        }
+    },
+})
 
--- 
+-- Give the signature help a rounded border
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help, {
+    border = "rounded"
+  }
+)
 
--- vim.api.nvim_set_keymap('n', '<leader>c',
---                         '<C-w>v<C-w>l:e $HOME/.config/nvim/init.lua<cr>',
---                         {noremap = true, silent = true})
--- vim.api.nvim_set_keymap('n', '<leader>r', ':source $MYVIMRC<cr>',
---                         {noremap = true, silent = true})
--- vim.api.nvim_set_keymap('n', '<leader>f', ':Telescope find_files<cr>',
---                         {noremap = true, silent = true})
--- vim.api.nvim_set_keymap('n', '<leader>g', ':Telescope live_grep<cr>',
---                         {noremap = true, silent = true})
--- vim.api.nvim_set_keymap('n', '<leader>t', ':!pytest tests -x<cr>',
---                         {noremap = true, silent = true})
--- vim.api.nvim_set_keymap('n', '<leader>m', ':!bash run-tests<cr>',
---                         {noremap = true, silent = true})
--- vim.api.nvim_set_keymap('n', '<leader>"', 'i""""""<Esc>hhi ',
---                         {noremap = true, silent = true})
--- vim.api.nvim_set_keymap('t', '<C-n>', [[<C-\><C-n>]],
---                         {noremap = true, silent = true})
--- vim.api.nvim_set_keymap('n', '<leader>b',
---                         '<Cmd>lua require("dap").toggle_breakpoint()$cr$', {})
--- vim.api.nvim_set_keymap('n', '<leader>b',
---                         '<Cmd>lua require("dap").toggle_breakpoint()<cr>', {})
--- vim.api.nvim_set_keymap('n', '<F1>', '<Cmd>lua require("dap").step_over()<cr>',
---                         {})
--- vim.api.nvim_set_keymap('n', '<F2>', '<Cmd>lua require("dap").step_into()<cr>',
---                         {})
--- vim.api.nvim_set_keymap('n', '<F3>', '<Cmd>lua require("dap").step_out()<cr>',
---                         {})
--- vim.api.nvim_set_keymap('n', '<F5>', '<Cmd>lua require("dap").continue()<cr>',
---                         {})
--- vim.api.nvim_set_keymap('n', '<F12>', ':DapTerminate<cr>', {})
--- vim.api.nvim_set_keymap('n', 's', ':w<cr>', {})
--- vim.cmd.highlight("default link IndentLine Comment")
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gh', vim.lsp.buf.hover, opts)
+    vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', 'gn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', 'g[', function(opts) vim.diagnostic.goto_next({float = false}) end, opts)
+    vim.keymap.set('n', 'g]', function(opts) vim.diagnostic.goto_prev({float = false}) end, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, opts)
+  end,
+})
 
-
+-- copy selection to system clipboard
 vim.api.nvim_set_keymap('v', '<leader>y', '"+y', {noremap = true, silent = true})
