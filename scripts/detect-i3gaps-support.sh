7fffde1e1d8da5cd0cd27f3d65d921fd28c09149
Jim Rybarski <jim@rybarski.com>
2024-04-18 07:17:30 -0400

    i3 broke my gaps detection

--- scripts/detect-i3gaps-support.sh
+++ scripts/detect-i3gaps-support.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Check if gaps are supported
-gaps_support=$(i3 --version | grep -o "gaps")
+gaps_support=$(lsb_release -a | grep Arch)
 
 # If gaps are supported, enable them
 if [[ -n "$gaps_support" ]]; then
     cat <<EOF
     gaps inner 8px
     gaps outer 0px
-    EOF
+EOF
 fi
