f1705fda7db7150522c828c896216a32dcfa1253
Jim Rybarski <jim@rybarski.com>
2024-07-03 22:27:20 -0400

    fill autocompletion to last common character

---
+++ .config/fish/conf.d/completion.fish
@@ -0,0 +1,4 @@
+# fish_complete_path set to 1 enables path completion that autofills all common characters.
+set -U fish_complete_path 1
+# fish_complete_scope set to 2 ensures that the completion engine uses all common characters in the completions.
+set -U fish_complete_scope 2
