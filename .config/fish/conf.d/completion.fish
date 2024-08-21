42a2d92a06aecb08a79c37dc023fc4615544b26b
Jim Rybarski <jim@rybarski.com>
2024-08-20 21:47:27 -0400

    Fix annoying fish autocomplete.

--- .config/fish/conf.d/completion.fish
+++ .config/fish/conf.d/completion.fish
@@ -1,4 +1,8 @@
-# fish_complete_path set to 1 enables path completion that autofills all common characters.
-set -U fish_complete_path 1
-# fish_complete_scope set to 2 ensures that the completion engine uses all common characters in the completions.
+# Don't do annoying fish completion that cycles through options and doesn't fully complete paths.
+function fish_user_key_bindings
+    bind -M insert \t 'commandline -f complete; commandline -f repaint'
+end
+
+set -g fish_complete_cycle false
+set -U fish_complete_path 0
 set -U fish_complete_scope 2
