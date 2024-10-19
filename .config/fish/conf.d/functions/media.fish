03b9f9eca55464da8e7c40615f4ddbfc913e8a8f
Jim Rybarski <jim@rybarski.com>
2024-10-18 23:50:55 -0400

    fix archive function that was deleting the video

--- .config/fish/conf.d/functions/media.fish
+++ .config/fish/conf.d/functions/media.fish
@@ -12,9 +12,9 @@ end
 
 function archive
     set URL "$argv"
-    set VIDEO_PATH "$HOME"
-    set MUSIC_PATH "$HOME"
+    set VIDEO_PATH "$HOME/Videos"
+    set MUSIC_PATH "$HOME/Music"
     set FILENAME ("$HOME/.local/ytdlenv/bin/yt-dlp" --get-filename -o "$VIDEO_PATH/%(title)s.%(ext)s" "$URL")
-    "$HOME/.local/ytdlenv/bin/yt-dlp" -o "$FILENAME" "$URL"
     "$HOME/.local/ytdlenv/bin/yt-dlp" -x --enable-file-urls --audio-format mp3 -o "$MUSIC_PATH/%(title)s.%(ext)s" -- "file:$FILENAME"
+    "$HOME/.local/ytdlenv/bin/yt-dlp" -o "$FILENAME" "$URL"
 end
