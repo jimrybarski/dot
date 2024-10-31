0fa80cda49245dd068fc7aaff6d94f4a2b0a6d2b
Jim Rybarski <jim@rybarski.com>
2024-10-31 19:22:33 -0400

    disable modeline

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -66,3 +66,4 @@ vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decr
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
+vim.o.modeline = false -- prevent warning with files containing the text "vim:"
