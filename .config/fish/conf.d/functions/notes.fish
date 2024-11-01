915972060cf560ef3a12e71307af53e307f774a4
Jim Rybarski <jim@rybarski.com>
2024-11-01 10:24:00 -0400

    local notes

--- .config/fish/conf.d/functions/notes.fish
+++ .config/fish/conf.d/functions/notes.fish
@@ -1,5 +1,4 @@
 #/usr/bin/env fish
-# Edit markdown notes at remote server with fish/neovim
 
 function derive_note_filename
     set filename $argv[1]
@@ -13,19 +12,10 @@ end
 function edit_note
     set filename (derive_note_filename $argv[1] $argv[2])
     set directory (dirname $filename)
-    set osname (uname)
-    if test "$osname" != "Darwin"
-        ssh notes -t "/usr/local/bin/zsh -ic 'mkdir -p ~/$directory && v ~/$filename'"
-    else
-        mkdir -p "$HOME/notes/$directory" && v "$HOME/notes/$filename"
-    end
+    mkdir -p "$HOME/notes/$directory" && v "$HOME/notes/$filename"
 end
 
 function echo_note
     set filename (derive_note_filename $argv[1] $argv[2])
-    if test "$osname" != "Darwin"
-        ssh notes cat "/root/$filename"
-    else
-        cat "$HOME/notes/$filename"
-    end
+    cat "$HOME/notes/$filename"
 end
