f0a2d5358ae1516f76562a9547cc63486a612af5
Jim Rybarski <jim@rybarski.com>
2024-12-31 12:20:43 -0500

    More or less finalize bioinformatics plugin setup
    Length, GC content and pairwise alignment are all in pretty good shape
    now. There are normal and visual mode versions of everything.

--- .config/nvim/lua/science.lua
+++ .config/nvim/lua/science.lua
@@ -1,33 +1,30 @@
 bio = require("bioinformatics")
 
-function set_query_visual() 
+function set_query_visual()
     local seq = bio.get_visual_selection()
-    bio.set_pairwise_query(seq) 
+    bio.set_pairwise_query(seq)
 end
 
-function set_subject_visual() 
+function set_subject_visual_and_align()
     local seq = bio.get_visual_selection()
     bio.set_pairwise_subject(seq)
-end
-
-function set_query_current_word()
-    local seq = vim.fn.expand("<cword>")
-    bio.set_pairwise_query(seq) 
-end
-
-function set_subject_current_word()
-    local seq = vim.fn.expand("<cword>")
-    bio.set_pairwise_subject(seq) 
-end
-
-function set_subject_current_word_and_align()
-    local seq = vim.fn.expand("<cword>")
-    bio.set_pairwise_subject(seq) 
     local alignment = bio.pairwise_align()
     bio.display_text(alignment)
 end
 
-function run_alignment()
+function set_query_current_word()
+    local seq = vim.fn.expand("<cword>")
+    bio.set_pairwise_query(seq)
+end
+
+function set_subject_current_word()
+    local seq = vim.fn.expand("<cword>")
+    bio.set_pairwise_subject(seq)
+end
+
+function set_subject_current_word_and_align()
+    local seq = vim.fn.expand("<cword>")
+    bio.set_pairwise_subject(seq)
     local alignment = bio.pairwise_align()
     bio.display_text(alignment)
 end
@@ -40,9 +37,19 @@ function popup_stats()
     vim.notify(text)
 end
 
-vim.keymap.set('v', '<leader>bva', set_query_visual, { noremap = true, silent = true })
-vim.keymap.set('v', '<leader>bvb', set_subject_visual, { noremap = true, silent = true })
-vim.keymap.set('v', '<leader>bvs', popup_stats, { noremap = true, silent = true })
+function popup_stats_current_word()
+    local seq = vim.fn.expand("<cword>")
+    local gc_content = bio.gc_content_biotools(seq)
+    local length = bio.length_biotools(seq)
+    local text = string.format("GC: %.6f\nLen: %d bp", gc_content, length)
+    vim.notify(text)
+end
+
 vim.keymap.set('n', '<leader>ba', set_query_current_word, { noremap = true, silent = true })
+vim.keymap.set('v', '<leader>ba', set_query_visual, { noremap = true, silent = true })
+
 vim.keymap.set('n', '<leader>bb', set_subject_current_word_and_align, { noremap = true, silent = true })
-vim.keymap.set({"n", "v"}, '<leader>bp', run_alignment, { noremap = true, silent = true })
+vim.keymap.set('v', '<leader>bb', set_subject_visual_and_align, { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>bs', popup_stats_current_word, { noremap = true, silent = true })
+vim.keymap.set('v', '<leader>bs', popup_stats, { noremap = true, silent = true })
