aa5d3d0f8d76f8504173963d56c835387e749055
Jim Rybarski <jim@rybarski.com>
2025-05-26 09:51:53 -0400

    add scripts dir to path

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -15,7 +15,7 @@ set -x LC_TIME "en_US.UTF-8"
 set -x XDG_CONFIG_HOME "$HOME/.config"
 set -x LIBCLANG_PATH "/home/jim/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-18.1.2_20240912/esp-clang/lib"
 
-set -U fish_user_paths $HOME/.local/sra/bin $HOME/.cargo/bin $HOME/.local/bin /usr/local/go/bin $HOME/.rustup/toolchains/esp/xtensa-esp-elf/esp-14.2.0_20240906/xtensa-esp-elf/bin $HOME/.luarocks/bin $fish_user_paths
+set -U fish_user_paths $HOME/.local/sra/bin $HOME/.cargo/bin $HOME/.local/bin /usr/local/go/bin $HOME/scripts $HOME/.rustup/toolchains/esp/xtensa-esp-elf/esp-14.2.0_20240906/xtensa-esp-elf/bin $HOME/.luarocks/bin $fish_user_paths
 
 set -x SHELL $HOME/.local/bin/fish
 
