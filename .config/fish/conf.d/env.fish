a9aa9b3cf9c5e6b5cfff6076a1250a671c807e82
Jim Rybarski <jim@rybarski.com>
2025-08-08 10:33:26 -0400

    fix more paths

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -12,16 +12,23 @@ set -x LC_NUMERIC "en_US.UTF-8"
 set -x LC_PAPER "en_US.UTF-8"
 set -x LC_TELEPHONE "en_US.UTF-8"
 set -x LC_TIME "en_US.UTF-8"
+
 if test -d /opt/dot
+    # work config
     set -x XDG_CONFIG_HOME /opt/dot/.config
+    set -x XDG_DATA_HOME /opt/local/data
+    set -x XDG_STATE_HOME /opt/local/state
+    set -x XDG_CACHE_HOME /opt/local/cache
 else
     set -x XDG_CONFIG_HOME "$HOME/.config"
+    set -x LIBCLANG_PATH "/home/jim/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-18.1.2_20240912/esp-clang/lib"
 end
-set -x LIBCLANG_PATH "/home/jim/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-18.1.2_20240912/esp-clang/lib"
 
-set -U fish_user_paths $HOME/.local/sra/bin $HOME/.cargo/bin $HOME/.local/bin /usr/local/go/bin $HOME/scripts $HOME/.rustup/toolchains/esp/xtensa-esp-elf/esp-14.2.0_20240906/xtensa-esp-elf/bin $HOME/.luarocks/bin $HOME/.local/npm-global/bin $fish_user_paths
-
-set -x SHELL $HOME/.local/bin/fish
+if test -d /opt/dot
+    set -U fish_user_paths /opt/local/.cargo/bin /opt/local/bin /usr/local/go/bin /opt/local/.luarocks/bin /opt/local/npm-global/bin $fish_user_paths
+else
+    set -U fish_user_paths $HOME/.local/sra/bin $HOME/.cargo/bin $HOME/.local/bin /usr/local/go/bin $HOME/scripts $HOME/.rustup/toolchains/esp/xtensa-esp-elf/esp-14.2.0_20240906/xtensa-esp-elf/bin $HOME/.luarocks/bin $HOME/.local/npm-global/bin $fish_user_paths
+end
 
 # set my editor with fallback options in case I'm in a new environment
 set -l editors nvim vim vi nano
