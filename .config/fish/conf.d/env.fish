79aeec863f261f9eb925e086dfb1bb030150f146
Jim Rybarski <jim@rybarski.com>
2025-08-08 09:45:31 -0400

    dynamically choose config

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -12,7 +12,11 @@ set -x LC_NUMERIC "en_US.UTF-8"
 set -x LC_PAPER "en_US.UTF-8"
 set -x LC_TELEPHONE "en_US.UTF-8"
 set -x LC_TIME "en_US.UTF-8"
-set -x XDG_CONFIG_HOME "$HOME/.config"
+if test -d /opt/dot
+    set -x XDG_CONFIG_HOME /opt/dot/.config
+else
+    set -x XDG_CONFIG_HOME "$HOME/.config"
+end
 set -x LIBCLANG_PATH "/home/jim/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-18.1.2_20240912/esp-clang/lib"
 
 set -U fish_user_paths $HOME/.local/sra/bin $HOME/.cargo/bin $HOME/.local/bin /usr/local/go/bin $HOME/scripts $HOME/.rustup/toolchains/esp/xtensa-esp-elf/esp-14.2.0_20240906/xtensa-esp-elf/bin $HOME/.luarocks/bin $HOME/.local/npm-global/bin $fish_user_paths
