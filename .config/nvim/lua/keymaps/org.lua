c42328f76a18eafa0923a447f0dfa7a414dead01
Jim Rybarski <jim@rybarski.com>
2025-10-21 22:21:48 -0400

    blog post idea shortcut

--- .config/nvim/lua/keymaps/org.lua
+++ .config/nvim/lua/keymaps/org.lua
@@ -1,2 +1,3 @@
 -- Open my global todo list
 vim.keymap.set("n", ",t", ":e ~/notes/info/todo.org<cr>", { desc = "Open my global todo list" })
+vim.keymap.set("n", ",b", ":e ~/notes/info/blog.org<cr>GA", { desc = "Open my blog post idea list" })
