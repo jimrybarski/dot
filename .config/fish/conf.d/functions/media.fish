865815ae2e6da451ebceb4d0cfeb01a7a0383740
Jim Rybarski <jim@rybarski.com>
2026-07-26 16:41:42 -0400

    fix broken downloader

--- .config/fish/conf.d/functions/media.fish
+++ .config/fish/conf.d/functions/media.fish
@@ -1,22 +1,13 @@
 function vid
-    "$HOME/.local/ytdlenv/bin/yt-dlp" -o '$HOME/%(title)s.%(ext)s' "$argv"
+    "$HOME/.local/ytdlenv/bin/yt-dlp" --no-cookies --impersonate chrome --embed-thumbnail -o '$HOME/%(title)s.%(ext)s' "$argv"
 end
 
 function mp3
-    "$HOME/.local/ytdlenv/bin/yt-dlp" -x --audio-format mp3 -o '%(title)s.%(ext)s' "$argv"
+    "$HOME/.local/ytdlenv/bin/yt-dlp" --no-cookies --impersonate chrome -x --audio-format mp3 -o '%(title)s.%(ext)s' "$argv"
 end
 
 function vid2mp3
-    "$HOME/.local/ytdlenv/bin/yt-dlp" -x --audio-format mp3 --enable-file-urls -o '$HOME/%(title)s.%(ext)s' -- "file:$HOME/%(title)s.%(ext)s"
-end
-
-function archive
-    set URL "$argv"
-    set VIDEO_PATH "$HOME/Videos"
-    set MUSIC_PATH "$HOME/Music"
-    set FILENAME ("$HOME/.local/ytdlenv/bin/yt-dlp" --get-filename -o "$VIDEO_PATH/%(title)s.%(ext)s" "$URL")
-    "$HOME/.local/ytdlenv/bin/yt-dlp" -x --enable-file-urls --audio-format mp3 -o "$MUSIC_PATH/%(title)s.%(ext)s" -- "file:$FILENAME"
-    "$HOME/.local/ytdlenv/bin/yt-dlp" -o "$FILENAME" "$URL"
+    "$HOME/.local/ytdlenv/bin/yt-dlp" --no-cookies --impersonate chrome -x --audio-format mp3 --enable-file-urls -o '$HOME/%(title)s.%(ext)s' -- "file:$HOME/%(title)s.%(ext)s"
 end
 
 function pastevid
